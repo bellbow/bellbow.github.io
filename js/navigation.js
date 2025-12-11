5078c7ce24ee97819be15fe33300c2ce6ee6af0b
Bella Ball <mohawkball@gmail.com>
2025-12-10 18:01:26 -0600

    education and skills

--- js/navigation.js
+++ js/navigation.js
@@ -1,4 +1,4 @@
-
+//Nav Menu Toggle
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 
@@ -6,3 +6,33 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navLinks.classList.toggle('active');
 });
+
+//Toggle Theme
+const themeBtn = document.querySelector('.theme-btn');
+themeBtn.addEventListener('click',() =>{
+    let element = document.body;
+    element.classList.toggle('dark-mode')
+});
+//Skill Group Select
+const carouselItem = document.querySelectorAll('.carousel-item');
+const skillList = document.querySelectorAll('.skill-list');
+
+carouselItem.forEach(item => {
+    item.addEventListener('click', function() {
+        const category = this.id;
+
+        carouselItem.forEach(item => {
+            item.classList.remove('active');
+        });
+        skillList.forEach(item => {
+            item.classList.remove('active');
+        });
+
+        const itemsToActivate = document.querySelectorAll(`.${category}`);
+
+        itemsToActivate.forEach(item => {
+            item.classList.add('active');
+        });
+    });
+});
+
